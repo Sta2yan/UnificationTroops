9dbbe83b67b716ea194bbf5e49aafda20989412e
Sta2yan <s2_stas@mail.ru>
2022-03-12 18:50:29 +0300

    Добавьте файлы проекта.

---
+++ UnificationTroops/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnificationTroops
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            List<Soldier> soldiers1 = new List<Soldier>()
+            {
+                new Soldier("Борис"),
+                new Soldier("Антон"),
+                new Soldier("Андрей"),
+                new Soldier("Биткоин"),
+                new Soldier("Арсен"),
+            };
+
+            List<Soldier> soldiers2 = new List<Soldier>();
+            char searchLetter = 'б';
+
+            soldiers2 = soldiers2.Union(soldiers1.Where(solider => solider.Name.ToLower().StartsWith(searchLetter))).ToList();
+
+            foreach (var soldier in soldiers2)
+            {
+                soldier.ShowInfo();
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public string Name { get; private set; }
+
+        public Soldier(string name)
+        {
+            Name = name;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Солдат - {Name}");
+        }
+    }
+}
